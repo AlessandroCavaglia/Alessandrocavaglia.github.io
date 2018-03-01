8642c4bdcbb92820b7f8cc37fb4ed710c1d1363f
Alessandro Cavaglià <cavagliascuola@gmail.com>
2018-03-01 20:28:27 +0100

    Loading the layout

---
+++ test slider.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="CSS/Style.css">
+</head>
+<body>
+<div class="slider" id="slider">
+    <div class="holder">
+        <div class="slide" id="slide-0"><span class="temp">74°</span></div>
+        <div class="slide" id="slide-1"><span class="temp">64°</span></div>
+        <div class="slide" id="slide-2"><span class="temp">82°</span></div>
+    </div>
+</div>
+</body>
+</html>
